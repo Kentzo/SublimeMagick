9b86848d3b7ce409fa48cb886ac938dd49383d4a
Ilya Kulakov <kulakov.ilya@gmail.com>
2012-06-17 18:19:50 +0700

    Allow a user to configure path to the identify tool.

--- SublimeMagick.py
+++ SublimeMagick.py
@@ -9,13 +9,14 @@ class SublimeMagick(sublime_plugin.EventListener):
     def on_activated(self, view):
         file_name = view.file_name()
         if file_name and len(file_name):
-            format = self.settings.get('sublimemagick_format', "%w x %h")
-            infos = self.identify(file_name, format)
+            infos = self.identify(file_name)
             if len(infos):
                 sublime.status_message(infos[0])
 
-    def identify(self, file_name, format):
-        infos = self.run_shell('identify -format "{0}\n" {1}'.format(format, file_name))
+    def identify(self, file_name):
+        identify_path = self.settings.get('sublimemagick_identify_path', "env identify")
+        format = self.settings.get('sublimemagick_format', "%w x %h")
+        infos = self.run_shell('{0} -format "{1}\n" {2}'.format(identify_path, format, file_name))
         return [info.strip() for info in infos]
 
     @staticmethod
